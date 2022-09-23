ff169488328662a11dfdacaae0b96a7c7b68ed70
Slience <2890554485@qq.com>
2022-09-23 10:11:06 +0800

    1. leetcode 9/23

---
+++ src/checkBalance/TreeNode.java
@@ -0,0 +1,22 @@
+package checkBalance;
+
+/**
+ * @author Slience
+ * @version 1.0
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x;}
+}
